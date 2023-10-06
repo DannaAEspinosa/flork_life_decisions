7211d721fee073aa9f4d695abae3bed586716bf7
CarlosJaBr <carjabo12@gmail.com>
2023-10-06 16:54:42 -0500

    feat_add_gui_test_file

---
+++ test/test_gui.py
@@ -0,0 +1,13 @@
+import unittest
+from src.gui import FlorkWindow,NuevoFinalDialog
+
+class FlorkWindowTest(unittest.TestCase):
+
+    def setUp(self):
+        self.window=FlorkWindow()
+
+class NuevoFinalDialogTest(unittest.TestCase):
+
+    def setUp(self):
+        self.nFinal=NuevoFinalDialog()
+    
