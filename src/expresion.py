fa574a843d969d882c9051fd29b903e93cae1ecd
Diana Balanta <dianalorenabalanta@gmail.com>
2023-10-06 15:26:32 -0500

    GUI has been modified

--- src/expresion.py
+++ src/expresion.py
@@ -6,14 +6,12 @@ class Expresion:
        self.expName = r'^[A-Z][a-zA-Z]{2,}$'
        self.expNewFinal =r'\b(Yo|creo|que|el|final|puede|mejorar|con)\b'
        
-    def get_expName(self):
-        return self.expName
-
-    def get_expNewFinal(self):
-        return self.expNewFinal
-    
     def validateName(self, name):
-        return re.match(self.expName,name)        
+        return re.match(self.expName,name)    
+
+           
+    def validateFinal(self, cadena):
+        return re.findall(self.expNewFinal, cadena)      
 
     
 
--- src/main.py
+++ src/main.py
@@ -1,14 +1,5 @@
-import gui 
+import juego 
 from transductor import Transductor
 
 if __name__ == '__main__':
-    gui.create_window()
-    transductor = Transductor()
-
-    cadena_entrada = "Nvk od oev lo frn rovuon ronblzddbn"
-    cadena_traducida = transductor.translate(cadena_entrada)
-    print(cadena_entrada)
-    # Imprime la cadena traducida
-    print(cadena_traducida)
-
-    
+    juego.create_window()
